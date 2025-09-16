4f96d7a45efcf7252e98f7bc899e7eea23926c02
anitawanjiku <mwanan3@furman.edu>
2025-09-15 22:42:58 -0400

    yes

--- src/Utilities/Containers/SymbolTable.cs
+++ src/Utilities/Containers/SymbolTable.cs
@@ -12,7 +12,6 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Text;
 using DataStructures;
-using Xunit.Abstractions;
 
 #nullable enable
 
@@ -23,12 +22,8 @@ namespace DataStructure
         DLL<TKey> dll_keys = new DLL<TKey>();
         DLL<TValue> dll_values = new DLL<TValue>();
 
-        // Gets or sets the element with the specified key.
-        // public TValue this[TKey key]
-        // {
-        //     get => GetNode;
-        //     set => ;
-        // }
+
+        public TValue this[TKey key] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
         private SymbolTable<TKey, TValue>? parent;
         public SymbolTable<TKey, TValue>? Parent
@@ -45,50 +40,34 @@ namespace DataStructure
 
         public bool IsReadOnly => throw new NotImplementedException();
 
-        // Add the key and values to their respective lists
         public void Add(TKey key, TValue value)
         {
-            if (key == null)
-            {
-                throw new ArgumentNullException();
-            }
-            // ArgumentException
-            dll_keys.Add(key);
-            dll_values.Add(value);
-
+            throw new NotImplementedException();
         }
 
-        // Add the key and values to their respective lists
         public void Add(KeyValuePair<TKey, TValue> item)
         {
-            // Access the key by going to the item first
-            dll_keys.Add(item.Key);
-            dll_values.Add(item.Value);
+            throw new NotImplementedException();
         }
 
-        // Clear the lists
         public void Clear()
         {
-            dll_keys.Clear();
-            dll_values.Clear();
+            throw new NotImplementedException();
         }
 
-        // Check if the key and value exists in their respective lists
         public bool Contains(KeyValuePair<TKey, TValue> item)
         {
-            dll_keys.Contains(item.Key);
-            dll_values.Contains(item.Value);
+            throw new NotImplementedException();
         }
 
-        // Check if the key exists in its list
         public bool ContainsKey(TKey key)
         {
-            dll_keys.Contains(key);
+            throw new NotImplementedException();
         }
 
         public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
         {
-            
+            throw new NotImplementedException();
         }
 
         public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
@@ -98,16 +77,12 @@ namespace DataStructure
 
         public bool Remove(TKey key)
         {
-            // What happens when the value doesnt have a corresponding key
-            // Find the value corresponding to the key then remove the value
-            dll_keys.Remove(key);
-
+            throw new NotImplementedException();
         }
 
         public bool Remove(KeyValuePair<TKey, TValue> item)
         {
-            dll_keys.Remove(item.Key);
-            dll_values.Remove(item.Value); 
+            throw new NotImplementedException();
         }
 
         public bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value)
@@ -119,5 +94,7 @@ namespace DataStructure
         {
             return GetEnumerator();
         }
+
+        
     }
 }
