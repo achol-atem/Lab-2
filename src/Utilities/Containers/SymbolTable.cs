b5c75582d731a0b35148a3c7c94c62c094e70ad0
achol-atem <atemac2@furman.edu>
2025-09-16 21:24:29 -0400

    Count() function, fixing some syntax errors, added skeleton of the 2 functions needed to be implemented for lab

--- src/Utilities/Containers/SymbolTable.cs
+++ src/Utilities/Containers/SymbolTable.cs
@@ -12,6 +12,7 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Text;
 using DataStructures;
+using Xunit.Abstractions;
 
 #nullable enable
 
@@ -22,47 +23,77 @@ namespace DataStructure
         DLL<TKey> dll_keys = new DLL<TKey>();
         DLL<TValue> dll_values = new DLL<TValue>();
 
-
-        public TValue this[TKey key] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        // Gets or sets the element with the specified key.
+        // public TValue this[TKey key]
+        // {
+        //     get => GetNode;
+        //     set => ;
+        // }
 
         private SymbolTable<TKey, TValue>? parent;
         public SymbolTable<TKey, TValue>? Parent
         {
             get { return parent; }
-
         }
 
         public ICollection<TKey> Keys => throw new NotImplementedException();
 
         public ICollection<TValue> Values => throw new NotImplementedException();
 
-        public int Count => throw new NotImplementedException();
+        // Return the number of elements in the DLL, only found keys because the lists are parallel
+        public int Count()
+        {
+            return dll_keys.Count();
+        }
 
-        public bool IsReadOnly => throw new NotImplementedException();
+        public bool IsReadOnly()
+        {
+            // dll_keys.IsReadOnly();
 
+        }
+
+        // Add the key and values to their respective lists
         public void Add(TKey key, TValue value)
         {
-            throw new NotImplementedException();
+            if (key == null || value == null)
+            {
+                throw new ArgumentNullException();
+            }
+            // ArgumentException
+            dll_keys.Add(key);
+            dll_values.Add(value);
+
         }
 
+        // Add the key and values to their respective lists
         public void Add(KeyValuePair<TKey, TValue> item)
-        {
-            throw new NotImplementedException();
+        {   
+            if (item.Key == null || item.Value == null)
+            {
+                throw new ArgumentNullException();
+            }
+            // Access the key by going to the item first
+            dll_keys.Add(item.Key);
+            dll_values.Add(item.Value);
         }
 
+        // Clear the lists
         public void Clear()
         {
-            throw new NotImplementedException();
+            dll_keys.Clear();
+            dll_values.Clear();
         }
 
+        // Check if the key and value exists in their respective lists
         public bool Contains(KeyValuePair<TKey, TValue> item)
         {
-            throw new NotImplementedException();
+            return dll_keys.Contains(item.Key) && dll_values.Contains(item.Value);
         }
 
+        // Check if the key exists in its list
         public bool ContainsKey(TKey key)
         {
-            throw new NotImplementedException();
+            return dll_keys.Contains(key);
         }
 
         public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
@@ -77,12 +108,15 @@ namespace DataStructure
 
         public bool Remove(TKey key)
         {
-            throw new NotImplementedException();
+            // What happens when the value doesnt have a corresponding key
+            // Find the value corresponding to the key then remove the value
+            return dll_keys.Remove(key);
+
         }
 
         public bool Remove(KeyValuePair<TKey, TValue> item)
         {
-            throw new NotImplementedException();
+            return dll_keys.Remove(item.Key) && dll_values.Remove(item.Value);
         }
 
         public bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value)
@@ -93,6 +127,20 @@ namespace DataStructure
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
+        }
+
+        // Checks for key existence only in the current scope
+        // Not traversing up to parent scopes
+        public bool ContainsKeyLocal(TKey key)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Attemps to retrieve a value only from the current scope
+        // No searching parent scopes
+        public bool TryGetValueLocal(TKey key, out TValue value)
+        {
+            throw new NotImplementedException();
         }
 
         
